76566f6d6b5b580013aa6cb5898e5557cf65ba08
Maximelebonm <lebon.maxime.m@gmail.com>
2022-02-05 17:37:48 +0100

    main

--- app1/src/Components/Header.jsx
+++ app1/src/Components/Header.jsx
@@ -1,13 +1,18 @@
 const Header = () => {
 
     return (
-        <div>
-            <h1>Test</h1>
-        </div>
+            <div className="container-fluid">
+                <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                    <a className="navbar-brand text-light" href="#">MusicLearn</a>
+                    
+                    <a className="nav-link text-light my-lg-0" href="#">MusicLearn</a>
+                  
+                </nav>
+            </div>
 
     )
-        
-    
+
+
 
 };
 
--- app1/src/Screen/BaseScreen.jsx
+++ app1/src/Screen/BaseScreen.jsx
@@ -3,7 +3,6 @@ const BaseScreen = () => {
 
     return (
         <div>
-            Ceci est un
             <Header/>
         </div>
     );
