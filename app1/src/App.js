2c1856dc3746d27011cdeb70ff9f684f73eeb94e
Maximelebonm <lebon.maxime.m@gmail.com>
2022-02-06 13:57:11 +0100

    y

--- app1/src/App.js
+++ app1/src/App.js
@@ -5,25 +5,26 @@ import ChoixInstrumentScreen from './Screens/ChoixInstrumentScreen';
 import HomeScreen from './Screens/HomeScreen';
 import NotFoundScreen from './Screens/NotFoundScreen';
 import AuthScreen from './Screens/AuthScreen';
+import ExplicationScreen from './Screens/ExplicationScreen';
+
 
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/authscreen" element={<AuthScreen />}/>
-       
-      </Routes>
-    </BrowserRouter>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<BaseScreen />}>
-        <Route index element={<HomeScreen />} />
-          <Route path="/Choixinstrument" element={<ChoixInstrumentScreen />} />
-          <Route path="*" element={<NotFoundScreen />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<AuthScreen />}/>
+            <Route path="/explication" element={<ExplicationScreen />} />
+
+   
+          <Route path="/" element={<BaseScreen />}>
+            <Route index element={<HomeScreen />} />
+            <Route path="/Choixinstrument" element={<ChoixInstrumentScreen />} />
+            <Route path="*" element={<NotFoundScreen />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ app1/src/Screens/ExplicationScreen.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+const ExplicationScreen = () => {
+
+    return (
+        <Link to="/auth"><h1>Page explication</h1></Link>
+
+    );
+};
+export default ExplicationScreen
